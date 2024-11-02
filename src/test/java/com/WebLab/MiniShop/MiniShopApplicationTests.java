e129030a32a6806a2891a740c7d9c87df1037b97
Jahidul Islam <jahidulzaid@gmail.com>
2024-11-03 02:00:49 +0600

    init project

---
+++ src/test/java/com/WebLab/MiniShop/MiniShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WebLab.MiniShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
